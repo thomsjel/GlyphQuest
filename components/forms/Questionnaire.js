c0bc23a0db9de83568e701e7acdbbf06bdc8fb19
thomsjel <it221506@fhstp.ac.at>
2024-06-10 10:43:45 +0200

    lshif

--- components/forms/Questionnaire.js
+++ components/forms/Questionnaire.js
@@ -150,7 +150,7 @@ function Questionnaire({ onEmailSending, onEmailSent, onForm, ...props }) {
           </label>
         </div>
 
-        {currentQuestion !== questions.length - 1 ? (
+        {currentQuestion !== questions.length ? (
           <button
             onClick={handleNextQuestion}
             disabled={
